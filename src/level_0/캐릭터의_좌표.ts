342e9e5697c355ac4a599f0c079e1e600f174117
D5ng <121039744+D5ng@users.noreply.github.com>
2026-02-16 18:55:23 +0900

    feat: 캐릭터의_좌표 구현 (#5)
    * feat: 캐릭터의_좌표 구현
    
    * refactor: 캐릭터의_좌표 1차 리팩토링
    
    - loopup table 활용
    - move 함수 추상화
    
    * refactor: lookup table에서 해당 x 및 y의 값만 담는게 아닌 데이터 자체를 전달하여 리팩토링
    
    * refactor: direction 상수를 상수 컨벤션에 맞도록 재정의
    
    * refactor: 변수, 함수 네이밍 변경 및 정책에 따른 변경 대처를 위한 코드 작성
    
    * refactor: 한계치를 기반으로 최소와 최대 값을 구하는 함수로 리팩토링
    
    ---------
    
    Co-authored-by: = <=>

---
+++ src/level_0/캐릭터의_좌표.ts
@@ -0,0 +1,68 @@
+/**
+ * 문제 설명
+ * 머쓱이는 RPG게임을 하고 있습니다. 게임에는 up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다.
+ * 예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다.
+ * 머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다. 캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
+ * [0, 0]은 board의 정 중앙에 위치합니다. 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
+ *
+ * 제한사항
+ * board은 [가로 크기, 세로 크기] 형태로 주어집니다.
+ * board의 가로 크기와 세로 크기는 홀수입니다.
+ * board의 크기를 벗어난 방향키 입력은 무시합니다.
+ * 0 ≤ keyinput의 길이 ≤ 50
+ * 1 ≤ board[0] ≤ 99
+ * 1 ≤ board[1] ≤ 99
+ * keyinput은 항상 up, down, left, right만 주어집니다.
+ *
+ * 입출력 예
+ * keyinput	board	result
+ * ["left", "right", "up", "right", "right"]	[11, 11]	[2, 1]
+ * ["down", "down", "down", "down", "down"]	[7, 9]	[0, -4]
+ */
+
+const DIRECTION_OFFSET: Record<string, [number, number]> = {
+	left: [-1, 0],
+	right: [1, 0],
+	up: [0, 1],
+	down: [0, -1],
+};
+
+type Coordinate = [x: number, y: number];
+
+// 최대 이동 범위를 계산하는 함수
+function calculateMaxMovementRange(boardDimensions: number[]): Coordinate {
+	const [width, height] = boardDimensions;
+	// 변경 가능성을 고려하여 map을 사용하지 않음.
+	// `boardDimensions.map((value) => Math.floor(value / 2))` 라는 코드를 작성해도 되지만, 정책이 변경된다면 이 코드는 사용할 수 없게됨.
+	return [Math.floor(width / 2), Math.floor(height / 2)];
+}
+
+function clampToLimit(value: number, limit: number): number {
+	return Math.max(-limit, Math.min(limit, value));
+}
+
+// keyinput에 따라 이동 범위를 계산하는 함수
+// 즉 최종 좌표를 계산하는 함수
+function calculateFinalCoordinates(keyinput: string[], movementBoundary: Coordinate): Coordinate {
+	const [maxX, maxY] = movementBoundary;
+
+	let currentX = 0;
+	let currentY = 0;
+
+	for (const key of keyinput) {
+		const [dx, dy] = DIRECTION_OFFSET[key];
+		currentX = clampToLimit(currentX + dx, maxX);
+		currentY = clampToLimit(currentY + dy, maxY);
+	}
+
+	return [currentX, currentY];
+}
+
+function 캐릭터의_좌표(keyinput: string[], board: number[]) {
+	const movementBoundary = calculateMaxMovementRange(board);
+	return calculateFinalCoordinates(keyinput, movementBoundary);
+}
+
+console.log(캐릭터의_좌표(["left", "right", "up", "right", "right"], [11, 11])); // [2, 1]
+console.log(캐릭터의_좌표(["down", "down", "down", "down", "down"], [7, 9])); // [0, -4]
+console.log(캐릭터의_좌표(["right", "right", "right", "right", "right", "left"], [9, 5])); // [3, 0]
