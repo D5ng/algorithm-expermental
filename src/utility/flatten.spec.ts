8b8a98d03afe1e338ea89138d793bea37328a7e4
d5ng <dong5ffice@gmail.com>
2026-03-11 01:36:08 +0900

    test: flatten 함수에 대한 단위 테스트 추가
    - flatten 함수의 다양한 깊이에 대한 평탄화 동작을 검증하는 테스트 케이스 추가
    - 기본값, 특정 깊이, 무한 깊이에 대한 테스트 포함

---
+++ src/utility/flatten.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+import { flatten } from "./flatten";
+
+describe("flatten", () => {
+	const nestedArr = [1, [2, [3, [4]]], 5];
+
+	it("depth를 넘겨주지 않았을 때 기본값 1로 한 단계만 평탄화한다", () => {
+		const flatted = flatten(nestedArr);
+		expect(flatted).toEqual([1, 2, [3, [4]], 5]);
+	});
+
+	it("depth가 2일 때 두 단계 평탄화한다", () => {
+		const flatted = flatten(nestedArr, 2);
+		expect(flatted).toEqual([1, 2, 3, [4], 5]);
+	});
+
+	it("depth가 Infinity일 때 모든 중첩 배열을 평탄화한다", () => {
+		const flatted = flatten(nestedArr, Infinity);
+		expect(flatted).toEqual([1, 2, 3, 4, 5]);
+	});
+});
