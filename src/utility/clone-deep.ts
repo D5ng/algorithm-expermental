93e96ceffafeb2b9a807ec9f3149ad47b25bdce8
d5ng <dong5ffice@gmail.com>
2026-03-12 23:17:02 +0900

    feat: cloneDeep 함수에 순환 참조 처리 추가
    - cloneDeep 함수에 WeakMap을 사용하여 순환 참조를 처리하는 로직 추가
    - 재귀 호출 시 동일한 객체를 참조할 경우 기존 객체를 반환하도록 수정

--- src/utility/clone-deep.ts
+++ src/utility/clone-deep.ts
@@ -8,9 +8,13 @@
  *    - **순환 참조 대응**: 객체 내부에 자기 자신을 가리키는 프로퍼티가 있어도 무한 루프에 빠지지 않아야 함 (힌트: `WeakMap` 활용).
  */
 
-export function cloneDeep<T>(sourceObject: T): T {
+export function cloneDeep<T>(sourceObject: T, hash = new WeakMap()): T {
 	if (!isReference(sourceObject)) {
 		return sourceObject;
+	}
+
+	if (hash.has(sourceObject)) {
+		return hash.get(sourceObject) as T;
 	}
 
 	if (isDate(sourceObject)) {
@@ -22,9 +26,10 @@ export function cloneDeep<T>(sourceObject: T): T {
 	}
 
 	const container = (Array.isArray(sourceObject) ? [] : {}) as T;
+	hash.set(sourceObject, container);
 
 	for (const [key, value] of Object.entries(sourceObject)) {
-		container[key] = cloneDeep(value);
+		container[key] = cloneDeep(value, hash);
 	}
 
 	return container;
