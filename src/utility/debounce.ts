48530536df8cf1ca76c5a5fdf9c8972fd5b859ed
d5ng <dong5ffice@gmail.com>
2026-04-20 23:20:44 +0900

    feat: debounce 함수 구현

---
+++ src/utility/debounce.ts
@@ -0,0 +1,23 @@
+/**
+ * Debounce 함수 구현 과제
+ * @param callback 실행할 타겟 함수
+ * @param delay 대기 시간 (ms)
+ * @returns 디바운스가 적용된 새로운 함수
+ */
+export function debounce<T extends (...args: any[]) => any>(
+  callback: T,
+  delay: number,
+) {
+  let timeoutId: ReturnType<typeof setTimeout | null> = null;
+
+  return function (this: any, ...args: Parameters<T>) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+
+    timeoutId = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
+}
