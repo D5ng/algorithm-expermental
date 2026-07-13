c84c0e13ffad9fa03afbee222b2c460950898fac
D5ng <121039744+D5ng@users.noreply.github.com>
2026-07-13 13:34:33 +0900

    feat: CustomPromise 구현 (#13)
    * feat: customPromise 구현 중
    
    - Promise class 내부 구현
    - then 메서드 구현
    
    * feat: catch 메서드 구현 및 테스트 케이스 통과
    
    * feat: static method resolve, reject 구현
    
    * refactor: type error 수정 및  status, result를 하나의 객체로 묶음

---
+++ src/utility/custom-promise.ts
@@ -0,0 +1,81 @@
+type PromiseStatus = "pending" | "fulfilled" | "rejected";
+
+type PromiseState<T> =
+  | { status: "pending" }
+  | { status: "fulfilled"; value: T }
+  | { status: "rejected"; reason: unknown };
+
+type OnFulfilled<T, R> = (value: T) => R | PromiseLike<R>;
+type PromiseReject = (reason?: unknown) => void;
+
+type Executor<T> = (
+  resolve: (value: T) => void,
+  rejected: PromiseReject,
+) => void;
+
+export class CustomPromise<T> {
+  private state: PromiseState<T>;
+
+  constructor(executor: Executor<T>) {
+    this.state = { status: "pending" };
+
+    executor(
+      (resolve) => this.resolve(resolve),
+      (reject) => this.reject(reject),
+    );
+  }
+
+  get status(): PromiseStatus {
+    return this.state.status;
+  }
+
+  get result(): T | unknown | undefined {
+    switch (this.state.status) {
+      case "fulfilled":
+        return this.state.value;
+      case "rejected":
+        return this.state.reason;
+      default:
+        return undefined;
+    }
+  }
+
+  then<R>(onFulfilled: OnFulfilled<T, R>, onRejected?: PromiseReject) {
+    return new CustomPromise<T>((resolve, reject) => {
+      queueMicrotask(() => {
+        if (this.state.status === "fulfilled") {
+          const result = onFulfilled(this.state.value) as T;
+          resolve(result);
+        }
+
+        if (this.state.status === "rejected") {
+          if (onRejected) {
+            onRejected(this.state.reason);
+          } else {
+            reject(this.state.reason);
+          }
+        }
+      });
+    });
+  }
+
+  catch(onRejected: PromiseReject) {
+    return this.then(() => {}, onRejected);
+  }
+
+  private resolve(value: T) {
+    this.state = { status: "fulfilled", value };
+  }
+
+  private reject(reason?: unknown) {
+    this.state = { status: "rejected", reason };
+  }
+
+  static resolve<T>(value: T): CustomPromise<T> {
+    return new CustomPromise((resolve) => resolve(value));
+  }
+
+  static reject(reason?: unknown): CustomPromise<never> {
+    return new CustomPromise((_, reject) => reject(reason));
+  }
+}
